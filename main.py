db992e5659cc9002f86a5d509004a1c15815297c
wuxubaiyang <wuxubaiyang@gmail.com>
2018-07-03 12:20:27 +0800

    +1

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from spider import DataParseSpider, DownloadSpider, TranslateSpider, UploadSpider
+
 # 入口
 if __name__ == "__main__":
+    print("开始解析数据")
+    DataParseSpider.parse_paging(DataParseSpider.START_PAGING_INDEX, True)
+    print("开始翻译分类名次")
+    TranslateSpider.collection_word()
+    print("开始下载壁纸")
+    DownloadSpider.download_file()
+    print("开始上传数据到bmob")
+    UploadSpider.page_upload(UploadSpider.START_PAGE, UploadSpider.MAX_BATCH)
+    print("开始上传壁纸到oss")
+    UploadSpider.upload_file()
+    print("数据处理完成")
     pass
--- spider/UploadSpider.py
+++ spider/UploadSpider.py
@@ -1,8 +1,11 @@
+import os
 import json
 import time
+import oss2
 import requests
 from model import BatchRequest
 from pymongo import MongoClient
+from concurrent.futures import ThreadPoolExecutor
 
 # 数据库
 db_client = MongoClient(host="127.0.0.1", port=27017)
@@ -10,17 +13,44 @@ db_collection = db_client.python_spider.hdqwalls_spider
 
 # 最大并发数
 MAX_WORKS = 20
+START_PAGE = 0
+MAX_BATCH = 50
 
 base_url = "https://api.bmob.cn/1/batch"
 headers = {"X-Bmob-Application-Id": "390b6a10efdb099c82520d7478ce4bab",
            "X-Bmob-REST-API-Key": "4b26a2e1112982c8f7bbd35168b9c72a",
            "Content-Type": "application/json"}
+# 当前图片缓存目录
+photos_cache_path = "/Volumes/移动城堡/original_image"
+# oss的accesskey路径
+oss_access_key_file = "/Users/jianghan/Documents/WallpaperProject/AccessKey.json"
 
 
 def get_o_clock_of_today():
     local_time = time.localtime(time.time())
     o_clock = time.mktime(time.strptime(time.strftime('%Y-%m-%d 00:00:00', local_time), '%Y-%m-%d %H:%M:%S'))
     return int(o_clock)
+
+
+def upload_file():
+    """
+    文件上传到oss
+    :return:
+    """
+    with open(oss_access_key_file, "r") as f:
+        access_key = json.loads(f.read(), encoding="UTF-8")
+    auth = oss2.Auth(access_key["AccessKeyId"], access_key["AccessKeySecret"])
+    bucket = oss2.Bucket(auth, access_key["Endpoint"], access_key["Bucket"])
+    with ThreadPoolExecutor(MAX_WORKS) as executor:
+        for root, dirs, files in os.walk(photos_cache_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.path.getctime(file_path) > get_o_clock_of_today():
+                    executor.submit(bucket.put_object_from_file, "original_image/" + file, file_path)
+                    print("正在上传", file)
+        executor.shutdown()
+        print("文件上传完成")
+    pass
 
 
 def page_upload(page, limit):
@@ -54,4 +84,5 @@ def page_upload(page, limit):
 # 入口
 if __name__ == "__main__":
     page_upload(0, 50)
+    upload_file()
     pass
